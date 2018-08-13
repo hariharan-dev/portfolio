23307239bd3f4a6f16a78f42ed39a57fcfbff11c
Hariharan <hariharan@tendercuts.in>
2018-08-14 00:00:29 +0530

    Added super heart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  // setting random whilte initializing itself
+  heartTop: Number = getRandomInt(100, window.innerHeight - 150);
+  heartLeft: Number = getRandomInt(100, window.innerWidth - 150);
+  heartColor: String = '#' + Math.floor(Math.random() * 16777215).toString(16);
+
+  ngOnInit() {
+    // generates new position and color to heart for evry 3secs.
+    setInterval(() => {
+      this.heartTop = getRandomInt(100, window.innerHeight - 150);
+      this.heartLeft = getRandomInt(100, window.innerWidth - 150);
+      this.heartColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    }, 3000);
+  }
+}
+
+export function getRandomInt(min, max) {
+  return Math.round(Math.random() * (max - min + 1)) + min;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
+import {RouterModule} from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
 
+import { rootRoutes } from './app.route';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rootRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
